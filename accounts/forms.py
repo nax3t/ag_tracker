c2fbe4920afda31395ef63da528071a0ca2cc671
Ian Schoonover <learntocodeinfo@gmail.com>
2025-01-07 21:28:04 -0600

    feat: enhance registration form with custom help texts and validation messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,6 +8,16 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        help_texts = {
+            'username': 'Letters, digits and @/./+/-/_ only.',
+            'email': 'A valid email address is required.',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Customize password help texts to be more concise
+        self.fields['password1'].help_text = "At least 8 characters. Mix letters and numbers."
+        self.fields['password2'].help_text = "Enter the same password again."
 
     def save(self, commit=True):
         user = super().save(commit=False)
